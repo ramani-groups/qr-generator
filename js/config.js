a34df99d51db526fefd9384f98b5ffd1aeb67726
Ramani Groups <ramanimgdigitalmarketing@gmail.com>
2026-08-10 16:05:42 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-window.RAMANI_QR_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbwqJkCywb_fNWBtbtjQ6hB_7MlL3DetwdO86XGYwbMNI-AhGlbkUY84Pj2Ld4Gd1jow/exec',
-  APP_NAME: 'Ramani QR Studio',
-  INTERNAL_ONLY: true
-});
