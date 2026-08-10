07c7b705dac6dca7b94cba6e813b4886e8703f5e
Ramani Groups <ramanimgdigitalmarketing@gmail.com>
2026-08-10 15:34:03 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.RAMANI_QR_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbxwWxd1c4-MRQ2Fa4APk02vo-iJuIvMu8cZijrhYbVPUKIls21FaM1xtvbVJS3cvA8Q/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwqJkCywb_fNWBtbtjQ6hB_7MlL3DetwdO86XGYwbMNI-AhGlbkUY84Pj2Ld4Gd1jow/exec',
   APP_NAME: 'Ramani QR Studio',
   INTERNAL_ONLY: true
 });
